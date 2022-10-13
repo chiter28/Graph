2550d5ba581cb487b2f8b199d724846c52793408
Dima <ivanowdima1999@gmail.com>
2022-10-13 12:26:27 +0300

    Добавьте файлы проекта.

---
+++ prog2/prog2.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+//ostream& operator << (ostream& os, istream& is)
+//{
+//	string s;
+//	is >> s;
+//	return os << s << " ";
+//}
+//
+//
+//int main()
+//{
+//	string file1, file2;
+//
+//	cout << "Enter name of first file: ";
+//	cin >> file1;
+//	ifstream in1{ file1 };
+//	
+//	cout << "Enter name of second file: ";
+//	cin >> file2;
+//	ifstream in2{ file2 };
+//
+//	ofstream out{ "out.txt" };
+//
+//	
+//
+//	
+//
+//	out << in1 << in2;
+//
+//
+//}
+
+template<typename T>
+void swap(T* a, T* b)
+{
+	T temp = *a;
+	*a = *b;
+	*b = temp;
+}
+ 
+int main()
+{
+	int a = 4;
+	int* p = &a;
+	int& r = *p;
+
+	cout << r;
+
+
+
+
+}
