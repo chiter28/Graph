7db59b4086e55edb99c1166a9a099ad4443b0b4e
Dima <ivanowdima1999@gmail.com>
2022-10-14 23:59:09 +0300

    pop

--- prog2/prog2.cpp
+++ prog2/prog2.cpp
@@ -34,22 +34,85 @@ using namespace std;
 //
 //}
 
-template<typename T>
-void swapi(T& a, T& b)
+void push_back(int*& arr, int& size, const int value)
 {
-	T temp = a;
-	a = b;
-	b = temp;
+	int* newArr = new int[size + 1];
+	for (int i = 0; i < size; i++) {
+		newArr[i] = arr[i];
+	}
+	newArr[size] = value;
+	delete[] arr;
+	arr = newArr;
+	size++;
 }
+
+void pop_back(int*& arr, int& size)
+{
+	int* newArr = new int[--size];
+	for (int i = 0; i < size; i++) {
+		newArr[i] = arr[i];
+	}
+	delete[] arr;
+	arr = newArr;
+}
+
+void push_begin(int*& arr, int& size, const int value)
+{
+	int* newArr = new int[++size];
+	newArr[0] = value;
+	for (int i = 0; i < size; i++) {
+		newArr[i + 1] = arr[i];
+	}
+	delete[] arr;
+	arr = newArr;
+}
+
+void pop_begin(int*& arr, int& size)
+{
+	int* newArr = new int[--size];
+	for (int i = 0; i < size; i++) {
+		newArr[i] = arr[i + 1];
+	}
+	delete[] arr;
+	arr = newArr;
+}
+
+void push_inpos(int*& arr, int& size, const int value, const int pos)
+{
+	bool is_push = false;
+	int* newArr = new int[++size];
+	for (int i = 0; i < size; i++) {
+		if (i == pos) {
+			newArr[i] = value;
+			is_push = true;
+			continue;
+		}
+		newArr[i] = arr[i - is_push];
+	}
+	delete[] arr;
+	arr = newArr;
+}
+
+void pop_inpos(int*& arr, int& size, const int pos)
+{
+	bool is_pop = false;
+	int* newArr = new int[--size];
+	for (int i = 0; i < size; i++) {
+		if (i == pos) {
+			is_pop = true;
+		}
+		newArr[i] = arr[i + is_pop];
+	}
+	delete[] arr;
+	arr = newArr;
+}
+
  
 int main()
 {
-	int* pa = new int;
-	*pa = 10;
 	
-	cout << pa << endl;
 
 
-
+	
 
 }
